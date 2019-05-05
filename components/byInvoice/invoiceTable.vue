b3c40ff12f3c2efa4db185a725fc988bb1a67e2b
Qirito <1075359710@qq.com>
2019-05-05 08:42:19 +0800

    发票管理流程组件初始化
    update

---
+++ components/byInvoice/invoiceTable.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="invoice">
+		<view class="flex-cncc"><image class="imgx300x80" src="https://zhushangimages.oss-cn-hangzhou.aliyuncs.com/invoice_electronic.png" mode=""></image></view>
+		<ul>
+			<li>
+				
+			</li>
+		</ul>
+	</view>
+	
+</template>
+
+<script>
+	
+</script>
+
+<style scoped>
+	.invoice{
+		width: 100vw;
+		height: 100%;
+		background-color: #FFFFFF;
+	}
+</style>
--- pages/tax/invoice/invoice.vue
+++ pages/tax/invoice/invoice.vue
@@ -1,15 +1,19 @@
 <template>
 	<view>
-		
+		<invoice-table></invoice-table>
 	</view>
 </template>
 
 <script>
+	import invoiceTable from "../../../components/byInvoice/invoiceTable.vue"
 	export default {
 		data() {
 			return {
 				
 			}
+		},
+		components:{
+			invoiceTable
 		},
 		methods: {
 			
